d3ea54ec3e53ea502af450bb4575478669902d03
aysf <ansufw@gmail.com>
2022-01-02 03:17:31 +0700

    Updates

---
+++ precache-manifest.fc093768ca07e510a756dbfc85e5072b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd1e47794d21261f86e7",
+    "url": "/monster-rolodex/static/css/main.a02863cb.chunk.css"
+  },
+  {
+    "revision": "dd1e47794d21261f86e7",
+    "url": "/monster-rolodex/static/js/main.08ec4413.chunk.js"
+  },
+  {
+    "revision": "735bbdcd14123abd057b",
+    "url": "/monster-rolodex/static/js/runtime~main.91733fea.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/monster-rolodex/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "b00b8e1d09328f33091c",
+    "url": "/monster-rolodex/static/js/3.1cea5788.chunk.js"
+  },
+  {
+    "revision": "e7edc84400cedcccb2dd356f1b9e59b6",
+    "url": "/monster-rolodex/index.html"
+  }
+];
